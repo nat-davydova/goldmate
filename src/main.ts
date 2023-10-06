f003cfe435f68325768c13ac85d3a65f8fbc2509
nat-davydova <natti.davydova@gmail.com>
2023-10-06 19:27:41 +0300

    feat: add dots and arrows to hero cards slider

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import "virtual:svg-icons-register";
 import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 // import Swiper and modules styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
+const swiper = new Swiper(".hero-swiper", {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination, Autoplay],
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  pagination: {
+    type: "bullets",
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
