d4c68889c46fcda80ce3a93708b81457c778f8da
nat-davydova <natti.davydova@gmail.com>
2023-10-06 18:19:42 +0300

    feat: add hero cards slider for smaller screens

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import "virtual:svg-icons-register";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
