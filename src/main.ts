f5582a8ba51966088d66a84949155521dc50a173
nat-davydova <natti.davydova@gmail.com>
2023-10-07 16:37:35 +0300

    refactor: outsource hero cards slider js code into separate module

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,11 @@
 import "virtual:svg-icons-register";
-import Swiper from "swiper";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
 // import Swiper and modules styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const swiper = new Swiper(".hero-swiper", {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination, Autoplay],
-  autoplay: {
-    delay: 5000,
-  },
-  loop: true,
-  pagination: {
-    type: "bullets",
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+import { initHeroCardsSlider } from "./modules/hero-cards-slider";
+
+window.addEventListener("load", () => {
+  initHeroCardsSlider();
 });
---
+++ src/modules/hero-cards-slider.ts
@@ -0,0 +1,21 @@
+import Swiper from "swiper";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+
+export const initHeroCardsSlider = () =>
+  new Swiper(".hero-swiper", {
+    // configure Swiper to use modules
+    modules: [Navigation, Pagination, Autoplay],
+    autoplay: {
+      delay: 5000,
+    },
+    loop: true,
+    pagination: {
+      type: "bullets",
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
