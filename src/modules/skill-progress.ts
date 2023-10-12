e3eb1dc6baec6cf6c61ebc9aab63407f314cbd16
nat-davydova <natti.davydova@gmail.com>
2023-10-12 19:25:51 +0300

    feat: hide found progress bar elements

--- src/modules/skill-progress.ts
+++ src/modules/skill-progress.ts
@@ -1,5 +1,5 @@
-// 1. найти все прогрессы в переданной обертке
-// 2. спрятать найденные прогрессы
+// !!!! 1. найти все прогрессы в переданной обертке
+// !!!! 2. спрятать найденные прогрессы
 // 3. создать новый прогресс-элемент
 // 3.1 общая обертка
 // 3.2 закрашенная область
@@ -9,5 +9,17 @@
 export function setSkillProgressBars(selector: string) {
   const skillsWrapper = document.querySelector(`${selector}`);
 
-  console.log(skillsWrapper);
+  if (!skillsWrapper) {
+    throw new Error("skillsWrapper not found");
+  }
+
+  const progressBars = skillsWrapper.querySelectorAll("progress");
+
+  if (!progressBars) {
+    return;
+  }
+
+  progressBars.forEach((elem) => elem.classList.add("js-hidden"));
+
+  console.log(progressBars);
 }
