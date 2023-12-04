6ea28ba3985b6e7c8a1caad6f6629397e98cf524
nat-davydova <natti.davydova@gmail.com>
2023-12-04 13:52:14 +0300

    refactor: rename variables into createCustomProgressBar

--- src/modules/skill-progress.ts
+++ src/modules/skill-progress.ts
@@ -1,11 +1,6 @@
-// !!!! 1. найти все прогрессы в переданной обертке
-// !!!! 2. спрятать найденные прогрессы
-// !!!!3. создать новый прогресс-элемент
-// !!!! 3.1 общая обертка
-// !!!! 3.2 закрашенная область
-// !!!!3.3 значение прогресса
-// 4. вставить в разметку
-// !!!!5. куда-то приделать проценты к прогрессу - дата-атрибут
+// 1. rename
+// 2. insert into html
+// 3. fix 100% value
 
 interface ICreateCustomProgressBarProps {
   value: string | null;
@@ -22,14 +17,16 @@ function createCustomProgressBar({
 
   const progressValue = Math.round(Number(value) / Number(max)) * 100;
 
-  const progressWrapper = document.createElement("div");
-  progressWrapper.classList.add("progress");
+  const progress = document.createElement("div");
+  progress.classList.add("progress");
 
   const progressBar = document.createElement("div");
   progressBar.classList.add("progress__bar");
   progressBar.setAttribute("data-progress-value", progressValue.toString());
 
-  progressWrapper.appendChild(progressBar);
+  progress.appendChild(progressBar);
+
+  console.log(progressValue);
 }
 
 export function setSkillProgressBars(selector: string) {
