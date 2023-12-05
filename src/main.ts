7fd0d4de59db81401976275196013e8d6762a578
nat-davydova <natti.davydova@gmail.com>
2023-12-05 18:40:04 +0300

    refactor: outsource progress bars setting to separate functions

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import { initHeroCardsSlider } from "./modules/hero-cards-slider";
-import { setSkillProgressBars } from "./modules/skill-progress.ts";
+import { setSkillProgressBars } from "./modules/skill-progress";
 
 window.addEventListener("load", () => {
   initHeroCardsSlider();
-  setSkillProgressBars(".about-skills");
+  setSkillProgressBars();
 });
---
+++ src/ui/progress/index.ts
@@ -0,0 +1,1 @@
+export { setProgressBars } from "./progress";
